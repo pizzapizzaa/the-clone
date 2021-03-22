0c4097b0ae5c0641410d19afd68087080faac623
pizzapizzaa <iamcheesepizzaa@gmail.com>
2021-03-22 23:18:23 +0800

    update bot code and python framework

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import os
-
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
+prefix = "liv"
+bot = commands.Bot(command_prefix=prefix)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
-bot = discord.Client()
-
+#Bot online message
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print("Livy's clone is online")
 
-
-# Handle the on member join event
+#Bot message history
 @bot.event
-async def on_member_join(member):
-    # Create a direct message
-    await member.create_dm()
-    # Send the message
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+async def on_message(message):
+    print("The message's content was", message.content)
+    await bot.process_commands(message)
 
+#Bot ping
+@bot.command()
+async def ping(ctx):
+    '''
+    Let you know the real-time latency
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    await ctx.send(latency)
+
+#Bot echo commands
+@bot.command()
+async def echo(ctx, *, content:str):
+    await ctx.send(content)
+
+#bot run
 bot.run(TOKEN)
